1d765b21e3913e848d110e58f1c5978292e0422f
siva kumar <sivakumarkatari2556@gmail.com>
2022-05-09 13:22:16 +0530

    first commit

---
+++ controllers/client/addWishlistItemController.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const eventData = require('../../data/events/client');
+
+const addWishlistItem = async (req,res,next) => {
+    try{
+        const params = req.body;
+        console.log(params);
+        const addWishlistItem = await eventData.addWishlistItem(params);
+        res.send(addWishlistItem);
+    } catch (error) {
+        res.status(422).send({
+            status:422,
+            message: error.message,
+        })
+    }
+}
+
+module.exports = {
+    addWishlistItem
+}
